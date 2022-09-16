6ccc369c5543f4eee177fcffe4eb15157d509518
Hau Van Tung <tunghv@galaxy.com.vn>
2022-09-16 09:07:07 +0000

    Update

---
+++ internal/conf/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Policies []Label `yaml:"Policies"`
+}
+type Policy struct {
+	Name   string  `yaml:"Name"`
+	APIKey string  `yaml:"APIKey"`
+	Labels []Label `yaml:"Labels"`
+}
+type Label struct {
+	Label string `yaml:"Label"`
+	Value string `yaml:"Value"`
+}
+
+
+// NewConfig returns a new decoded Config struct
+func NewConfig(configPath string) (*Config, error) {
+    // Create config structure
+    config := &Config{}
+
+    // Open config file
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    // Init new YAML decode
+    d := yaml.NewDecoder(file)
+
+    // Start YAML decoding from file
+    if err := d.Decode(&config); err != nil {
+        return nil, err
+    }
+
+    return config, nil
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"github.com/galaxyed/prometheus-proxy/internal/conf"
 )
 
 // NewProxy takes target host and creates a reverse proxy
