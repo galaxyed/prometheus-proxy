92e801205b9fdd8a4fca0d916effc25421c2ffdc
Hau Van Tung <tunghv@galaxy.com.vn>
2022-09-16 03:40:26 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+// NewProxy takes target host and creates a reverse proxy
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	originalDirector := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		modifyRequest(req)
+	}
+
+	proxy.ErrorHandler = errorHandler()
+	return proxy, nil
+}
+
+func modifyRequest(req *http.Request) {
+	if req.URL.Path == "/api/v1/label/__name__/values" {
+		q := req.URL.Query()
+		q.Add("match[]", "{project=\"DataV2\"}")
+		req.URL.RawQuery = q.Encode()
+	}
+
+	if req.URL.Path == "/api/v1/labels" {
+		q := req.URL.Query()
+		q.Add("match[]", "{project=\"DataV2\"}")
+		req.URL.RawQuery = q.Encode()
+	}
+
+	fmt.Println(req.URL.String())
+}
+
+func errorHandler() func(http.ResponseWriter, *http.Request, error) {
+	return func(w http.ResponseWriter, req *http.Request, err error) {
+		fmt.Printf("Got error while modifying response: %v \n", err)
+		return
+	}
+}
+
+func modifyResponse() func(*http.Response) error {
+	return func(resp *http.Response) error {
+		return errors.New("response body is invalid")
+	}
+}
+
+// ProxyRequestHandler handles the http request using proxy
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	// initialize a reverse proxy and pass the actual backend server url here
+	proxy, err := NewProxy("http://10.100.0.52:9090")
+	if err != nil {
+		panic(err)
+	}
+
+	// handle all requests to your server using the proxy
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
